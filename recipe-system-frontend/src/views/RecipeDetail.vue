7ccbd5bfab6a775b9a84b866841935034e9da7d8
24kHT <759110735@qq.com>
2026-05-30 20:11:42 +0800

    优化: RecipeCard整卡可点击 + 食材名称后配Q版小图标

--- recipe-system-frontend/src/views/RecipeDetail.vue
+++ recipe-system-frontend/src/views/RecipeDetail.vue
@@ -24,7 +24,7 @@
       <h2>食材</h2>
       <ul class="list">
         <li v-for="item in recipe.ingredients" :key="item.id || item.name">
-          <strong>{{ item.name }}</strong>
+          <strong>{{ item.name }}<img v-if="ingredientIcon(item.name)" :src="ingredientIcon(item.name)" class="ing-icon" /></strong>
           <span class="muted">{{ item.amount }}</span>
         </li>
       </ul>
@@ -74,6 +74,21 @@ const userStore = useUserStore();
 const recipe = ref(null);
 const loading = ref(false);
 const commentContent = ref("");
+
+const iconMap = {
+  "包菜": "baocai", "午餐肉": "wucanrou", "土豆": "tudou", "方便面": "fangbianmian",
+  "洋葱": "yangcong", "牛肉": "niurou", "猪肉": "zhurou", "番茄": "fanqie",
+  "白菜": "baicai", "白萝卜": "bailuobo", "米": "mi", "胡萝卜": "huoluobo",
+  "腊肠": "lachang", "花菜": "huacai", "芹菜": "qincai", "茄子": "qiezi",
+  "莴笋": "wosun", "菌菇": "jungu", "虾": "xia", "虾仁": "xiaren",
+  "西葫芦": "xihulu", "豆腐": "doufu", "面包": "mianbao", "面食": "mianshi",
+  "香肠": "xiangchang", "骨头": "gutou", "鸡肉": "jirou", "鸡蛋": "jidan", "黄瓜": "huanggua",
+};
+
+function ingredientIcon(name) {
+  const key = iconMap[name];
+  return key ? `/ingredient-icons/${key}.svg` : "";
+}
 
 async function loadDetail() {
   loading.value = true;
